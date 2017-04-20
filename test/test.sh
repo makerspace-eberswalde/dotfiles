c9825faf45b85b65b287c5ae64ea4b878e468fa1
divramod <divramod@gmail.de>
2017-04-20 04:53:29 +0200

    add test folder

---
+++ test/test.sh
@@ -0,0 +1,3 @@
+if [ ! -d ~/.oh-my-zsh ]; then
+  echo "in"
+fi
--- zsh/install-zsh.sh
+++ zsh/install-zsh.sh
@@ -2,7 +2,7 @@
 sudo apt-get install zsh
 
 # install oh-my-zsh: https://github.com/robbyrussell/oh-my-zsh
-if [ ! -d "~/.oh-my-zsh" ]; then
+if [ ! -d ~/.oh-my-zsh ]; then
   sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
