f30cb9b627340d23032b75a42e172e1914331925
divramod <divramod@gmail.de>
2017-04-20 07:35:19 +0200

    tmux

--- tmux/install-tmux.sh
+++ tmux/install-tmux.sh
@@ -1,5 +1,13 @@
-# install tmux
-sudo apt-get install tmux
+# install tmux from source for older linux versions: https://ubuntuincident.wordpress.com/2016/03/17/upgrade-tmux-to-the-latest-version/
+#sudo apt-get install tmux
+sudo apt-get install exuberant-ctags cmake libevent-dev libncurses5-dev
+cd /tmp
+wget https://github.com/tmux/tmux/releases/download/2.3/tmux-2.3.tar.gz
+tar -xzf tmux-2.3.tar.gz
+cd tmux-2.3
+.configure && make
+sudo make install
+rm -rf tmux*
 
 # link tmux.conf_setup
 rm -f ~/.tmux.conf_setup 2>/dev/null
