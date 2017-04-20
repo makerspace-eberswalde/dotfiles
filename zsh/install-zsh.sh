bd3669bc841df14b085734e28a536e16ae5c87fc
Arvid Petermann <divramod@gmail.com>
2017-04-20 05:58:04 +0200

    zsh

--- zsh/install-zsh.sh
+++ zsh/install-zsh.sh
@@ -1,5 +1,5 @@
 # install zsh
-# sudo apt-get install zsh
+sudo apt-get install zsh
 
 # install oh-my-zsh: https://github.com/robbyrussell/oh-my-zsh
 rm -rf ~/.oh-my-zsh
@@ -7,8 +7,17 @@ git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # mv .zshrc to backup if existing
 mv ~/.zshrc ~/.zshrc_backup 2>/dev/null
+rm -f ~/.zshrc_oh-my-zsh 2>/dev/null
 
-# link zshrc
-FILE=`pwd`/zshrc
-echo "link ~/.zshrc to $FILE" 
-ln -s $FILE ~/.zshrc
+# link oh-my-zsh 
+FILE_RC_OH=`pwd`/zshrc_oh-my-zsh
+ln -s $FILE_RC_OH ~/.zshrc_oh-my-zsh
+
+# link zshrc_setup which sources zshrc_oh-my-zsh
+FILE_RC_SETUP=`pwd`/zshrc_setup
+ln -s $FILE_RC_SETUP ~/.zshrc_setup
+
+# cp zshrc which sources zshrc_setup
+# write your own aliases here
+FILE_RC=`pwd`/zshrc
+cp $FILE_RC ~/.zshrc
