3b7e68508a697e796f5864160c67b009c2e537c4
divramod <divramod@gmail.de>
2017-04-20 04:52:04 +0200

    zshrc proof if ~/.oh-my-zsh exists

--- zsh/install-zsh.sh
+++ zsh/install-zsh.sh
@@ -2,7 +2,9 @@
 sudo apt-get install zsh
 
 # install oh-my-zsh: https://github.com/robbyrussell/oh-my-zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+if [ ! -d "~/.oh-my-zsh" ]; then
+  sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+fi
 
 # bakcup .zshrc to .zshrc_backup
 mv ~/.zshrc ~/.zshrc_backup
