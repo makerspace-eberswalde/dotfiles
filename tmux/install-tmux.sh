50fa1a92e3863b3d936be9e6381ea5fc9b0101ae
divramod <divramod@gmail.de>
2017-04-20 07:57:47 +0200

    tmux

--- tmux/install-tmux.sh
+++ tmux/install-tmux.sh
@@ -1,15 +1,8 @@
 # install tmux from source for older linux versions: https://ubuntuincident.wordpress.com/2016/03/17/upgrade-tmux-to-the-latest-version/
 # https://gist.github.com/P7h/91e14096374075f5316e
-#sudo apt-get update -yqqu
-sudo add-apt-repository -yu ppa:pi-rho/dev
-sudo apt-get update -yqqu
-sudo apt-get install -yqqu python-software-properties software-properties-common
-#sudo apt-get install -yqq tmux-next=2.3~20160913~bzr3547+20-1ubuntu1~ppa0~ubuntu16.04.1
-# sudo apt-get install -yqq tmux-next=2.3~20160913~bzr3547+20-1ubuntu1~ppa0~ubuntu15.10.1
-# sudo apt-get install -yqq tmux-next=2.3~20160913~bzr3547+20-1ubuntu1~ppa0~ubuntu15.04.1
- sudo apt-get install -yqq tmux-next=2.3~20160913~bzr3547+20-1ubuntu1~ppa0~ubuntu14.04.1
-# sudo apt-get install -yqq tmux-next=2.3~20160913~bzr3547+20-1ubuntu1~ppa0~ubuntu12.04.1
-tmux-next -V
+sudo apt-get install tmux-next
+sudo rm -f /usr/bin/tmux
+sudo ln -s /usr/bin/tmux-next /usr/bin/tmux
 
 # link tmux.conf_setup
 rm -f ~/.tmux.conf_setup 2>/dev/null
