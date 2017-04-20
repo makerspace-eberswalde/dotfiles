35e574dd844d281ad409e5ea3380193dff96c8ff
divramod <divramod@gmail.de>
2017-04-20 04:49:57 +0200

    zsh: link .zshrc

--- zsh/install-zsh.sh
+++ zsh/install-zsh.sh
@@ -3,3 +3,9 @@ sudo apt-get install zsh
 
 # install oh-my-zsh: https://github.com/robbyrussell/oh-my-zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# bakcup .zshrc to .zshrc_backup
+mv ~/.zshrc ~/.zshrc_backup
+
+# link zshrc
+ln -s zshrc ~/.zshrc
