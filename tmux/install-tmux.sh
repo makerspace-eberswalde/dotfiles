a32841844e70bdf88e8a39a3f37cf8b5a95c2681
divramod <divramod@gmail.de>
2017-04-20 07:21:39 +0200

    zshrc

--- tmux/install-tmux.sh
+++ tmux/install-tmux.sh
@@ -1,7 +1,12 @@
 # install tmux
 sudo apt-get install tmux
 
-# link tmux.conf
-rm -f ~/.gitconfig-setup 2>/dev/null
-FILE_CONFIG_SETUP=`pwd`/gitconfig-setup
-ln -s $FILE_CONFIG_SETUP ~/.gitconfig-setup
+# link tmux.conf_setup
+rm -f ~/.tmux.conf_setup 2>/dev/null
+FILE_CONFIG_SETUP=`pwd`/tmux.conf_setup
+ln -s $FILE_CONFIG_SETUP ~/.tmux.conf_setup
+
+# cp tmux.conf which sources tmux.conf_setup
+# write your own aliases here
+FILE_CONF=`pwd`/tmux.conf
+cp $FILE_CONF~/.tmux.conf
