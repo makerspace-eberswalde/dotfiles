23eb203d3e9d905d5aab78f91fdf502d09b14575
divramod <divramod@gmail.de>
2017-04-20 08:32:05 +0200

    tmux

--- tmux/install-tmux.sh
+++ tmux/install-tmux.sh
@@ -16,3 +16,8 @@ ln -s $FILE_CONFIG_SETUP ~/.tmux.conf_setup
 rm -f ~/.tmux.conf 2>/dev/null
 FILE_CONF=`pwd`/tmux.conf
 cp $FILE_CONF ~/.tmux.conf
+
+# TMUXINATOR: https://github.com/tmuxinator/tmuxinator
+# https://www.cyberciti.biz/faq/debian-ubuntu-install-gem-the-frontend-to-rubygems/
+sudo apt-get install ruby gem
+sudo gem install tmuxinator
