491bb390ac0224cb37320b161f05fd543d2941f2
divramod <divramod@gmail.de>
2017-04-20 06:04:11 +0200

    git

--- git/install-git.sh
+++ git/install-git.sh
@@ -1,1 +1,12 @@
-# TODO
+# TODO: create backups
+# TODO: source file for beeing able to have own things: http://stackoverflow.com/questions/1557183/is-it-possible-to-include-a-file-in-your-gitconfig
+
+# link 
+rm -f ~/.gitconfig 2>/dev/null
+FILE_CONFIG=`pwd`/gitconfig
+ln -s $FILE_CONFIG ~/.gitconfig
+
+# link 
+rm -f ~/.gitignore 2>/dev/null
+FILE_IGNORE=`pwd`/gitignore
+ln -s $FILE_IGNORE ~/.gitignore
