153d9443418a6b1863261f6087038f2f0194bb2e
divramod <divramod@gmail.de>
2017-04-20 06:16:57 +0200

    vim

--- vim/install-vim.sh
+++ vim/install-vim.sh
@@ -1,3 +1,14 @@
 sudo apt-get install vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+
+# link 
+rm -f ~/.vimrc 2>/dev/null
+FILE_RC=`pwd`/vimrc
+ln -s $FILE_RC ~/.vimrc
+
+# link 
+rm -f ~/.vimrc_setup 2>/dev/null
+FILE_RC_SETUP=`pwd`/vimrc_setup
+ln -s $FILE_RC_SETUP ~/.vimrc_setup
