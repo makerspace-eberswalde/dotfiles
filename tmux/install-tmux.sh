04f103bc111b80e418653a875e6687e4cbd82b09
divramod <divramod@gmail.de>
2017-04-20 07:22:17 +0200

    zshrc

--- tmux/install-tmux.sh
+++ tmux/install-tmux.sh
@@ -8,5 +8,6 @@ ln -s $FILE_CONFIG_SETUP ~/.tmux.conf_setup
 
 # cp tmux.conf which sources tmux.conf_setup
 # write your own aliases here
+rm -f ~/.tmux.conf 2>/dev/null
 FILE_CONF=`pwd`/tmux.conf
 cp $FILE_CONF~/.tmux.conf
