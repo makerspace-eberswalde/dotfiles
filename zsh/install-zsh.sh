d2a0bdea1f1371591610d2bdcc499b4e51589da6
divramod <divramod@gmail.de>
2017-04-20 05:08:30 +0200

    update install-zsh.sh

--- zsh/install-zsh.sh
+++ zsh/install-zsh.sh
@@ -3,12 +3,15 @@ sudo apt-get install zsh
 
 # install oh-my-zsh: https://github.com/robbyrussell/oh-my-zsh
 if [ ! -d ~/.oh-my-zsh ]; then
-  mv ~/.oh-my-zsh ~/.oh-my-zsh_backup
   sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
 # bakcup .zshrc to .zshrc_backup
 if [ -f ~/.zshrc ]; then
+  if [ -f ~/.zshrc_backup ]; then
+    rm ~/.zshrc_backup
+    mv ~/.zshrc ~/.zshrc_backup
+  fi
   mv ~/.zshrc ~/.zshrc_backup
 fi
 
