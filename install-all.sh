3170d38c6cc2bda6e7af3767efcb9899557f8038
Arvid Petermann <divramod@gmail.com>
2017-04-26 08:33:22 +0200

    add install-all.sh

---
+++ install-all.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/makerspace-eberswalde/dotfiles.git ~/.dotfiles
+
+#zsh
+cd ~/.dotfiles/zsh
+sh install-zsh.sh
---
+++ nodejs/install-nodejs.sh
@@ -0,0 +1,3 @@
+# https://askubuntu.com/questions/594656/how-to-install-the-latest-versions-of-nodejs-and-npm-for-ubuntu-14-04-lts
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y nodejs
