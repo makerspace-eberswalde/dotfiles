917ff1c8fe9d8a000f3f30396076b528e531e720
Arvid Petermann <divramod@gmail.com>
2017-04-26 08:37:36 +0200

    add install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,5 +1,22 @@
 git clone https://github.com/makerspace-eberswalde/dotfiles.git ~/.dotfiles
 
-#zsh
+# tmux
+cd ~/.dotfiles/tmux
+sh install-tmux.sh
+
+#zsh (depends on tmux)
 cd ~/.dotfiles/zsh
 sh install-zsh.sh
+
+#git
+cd ~/.dotfiles/git
+sh install-git.sh
+
+# nodejs
+cd ~/.dotfiles/nodejs
+sh install-nodejs.sh
+
+# vim
+cd ~/.dotfiles/vim
+sh install-vim.sh
+
