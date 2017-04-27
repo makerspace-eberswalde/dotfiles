09dc11b65e6a515baf3ecde8f3faae27dc672b94
Arvid Petermann <divramod@gmail.com>
2017-04-27 09:23:46 +0200

    update

--- cli/nodejs/install.sh
+++ cli/nodejs/install.sh
@@ -2,8 +2,11 @@ echo "\nINSTALL NODEJS"
 echo "===================================\n"
 
 # https://askubuntu.com/questions/594656/how-to-install-the-latest-versions-of-nodejs-and-npm-for-ubuntu-14-04-lts
-curl -o install-node-deb.sh https://deb.nodesource.com/setup_7.x
+echo "------------------------------------ CURL -------------------------------"
+curl -o install-node-deb.sh https://deb.nodesource.com/setup_7.x;
 ls -lisa
+echo "------------------------------------ install sh -------------------------------"
 sudo sh install-node-deb.sh;
 rm install-node-deb.sh
-sudo apt-get install -y nodejs
+echo "------------------------------------ apt-get -------------------------------"
+sudo apt-get install -y nodejs;
