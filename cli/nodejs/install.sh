4e376e204566be4898ac7daa8cbf0909f242010d
Arvid Petermann <divramod@gmail.com>
2017-04-27 11:19:35 +0200

    update

--- cli/nodejs/install.sh
+++ cli/nodejs/install.sh
@@ -3,11 +3,12 @@ echo "===================================\n"
 
 # https://askubuntu.com/questions/594656/how-to-install-the-latest-versions-of-nodejs-and-npm-for-ubuntu-14-04-lts
 echo "------------------------------------ CURL -------------------------------"
-curl -o install-node-deb.sh https://deb.nodesource.com/setup_7.x;
-ls -lisa;
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+#curl -o install-node-deb.sh https://deb.nodesource.com/setup_7.x;
+#ls -lisa;
 echo "------------------------------------ install sh -------------------------------"
-sudo sh install-node-deb.sh;
-rm install-node-deb.sh;
+#sudo sh install-node-deb.sh;
+#rm install-node-deb.sh;
 echo "------------------------------------ apt-get -------------------------------"
 sudo apt-get install -y nodejs;
-sudo apt-get install -f;
+#sudo apt-get install -f;
--- cli/zsh/install.sh
+++ cli/zsh/install.sh
@@ -2,25 +2,37 @@ echo "\nINSTALL ZSH"
 echo "===================================\n"
 
 # install zsh
+echo "\nzsh apt-get install"
+echo "---------------------\n"
 sudo apt-get install -y zsh;
 
 # install oh-my-zsh: https://github.com/robbyrussell/oh-my-zsh
+echo "\noh-my-zsh"
+echo "---------------------\n"
 rm -rf ~/.oh-my-zsh;
 git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh;
 
 # mv .zshrc to backup if existing
+echo "\n.zshrc_backup"
+echo "---------------------\n"
 mv ~/.zshrc ~/.zshrc_backup 2>/dev/null;
 rm -f ~/.zshrc_setup 2>/dev/null;
 
 # link zshrc_setup which sources zshrc_oh-my-zsh
+echo "\n.zshrc_setup"
+echo "---------------------\n"
 FILE_RC_SETUP=`pwd`/zshrc_setup;
 ln -s $FILE_RC_SETUP ~/.zshrc_setup;
 
 # link zshrc_setup which sources zshrc_oh-my-zsh
+echo "\n.zshrc_functions"
+echo "---------------------\n"
 FILE_RC_FUNCTIONS=`pwd`/zshrc_functions;
 ln -s $FILE_RC_FUNCTIONS ~/.zshrc_functions;
 
 # cp zshrc which sources zshrc_setup
 # write your own aliases here
+echo "\n.zshrc"
+echo "---------------------\n"
 FILE_RC=`pwd`/zshrc;
 cp -f $FILE_RC ~/.zshrc;
