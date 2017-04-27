800ff9f9e28cdcc281424d19eddadb406f2e1481
Arvid Petermann <divramod@gmail.com>
2017-04-27 09:10:53 +0200

    update

--- gui/google-chrome/install.sh
+++ gui/google-chrome/install.sh
@@ -1,4 +1,6 @@
-sudo apt-get install libxss1 libappindicator1 libindicator7
+sudo apt-get install -y libxss1
+sudo apt-get install -y libappindicator1
+sudo apt-get install -y libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 rm -rf google-chrome*.deb
--- install-gui.sh
+++ install-gui.sh
@@ -1,7 +1,11 @@
 # [INSTALL] google-chrome 
 cd ~/.dotfiles/gui/google-chrome
-sh install.sh
+sh install.sh;
 
 # [INSTALL] inkscape 
 cd ~/.dotfiles/gui/inkscape
-sh install.sh
+sh install.sh;
+
+# [INSTALL] blender 
+cd ~/.dotfiles/gui/blender
+sh install.sh;
