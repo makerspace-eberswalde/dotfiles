30b282e9bf6cce3c7795919cb401f2ec7c472a60
Arvid Petermann <divramod@gmail.com>
2017-04-28 08:36:52 +0200

    dropbox: update

--- gui/dropbox/install.sh
+++ gui/dropbox/install.sh
@@ -23,7 +23,6 @@ curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x
 sudo mkdir -p /opt/dropbox &&
 sudo tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox &&
 # link to account
-/opt/dropbox/dropboxd &&
 # Set Up Service Script
 cd ~ &&
 sudo curl -o /etc/init.d/dropbox https://gist.githubusercontent.com/thisismitch/d0133d91452585ae2adc/raw/699e7909bdae922201b8069fde3011bbf2062048/dropbox &&
@@ -32,8 +31,10 @@ sudo rm /etc/default/dropbox
 sudo touch /etc/default/dropbox
 sudo echo "DROPBOX_USERS=\"mkrspcebw\"" >> /etc/default/dropbox
 #sudo nano /etc/default/dropbox &&
-sudo service dropbox start &&
+sudo service dropbox start
 sudo update-rc.d dropbox defaults &&
+# run droxbox and link
+/opt/dropbox/dropboxd &&
 # Install Dropbox CLI
 
 
