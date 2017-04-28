8c80cee3d3a2a31c50f36d5e31039260883b1630
Arvid Petermann <divramod@gmail.com>
2017-04-28 20:44:02 +0200

    inkscape: add latest version install

--- gui/inkscape/install.sh
+++ gui/inkscape/install.sh
@@ -1,5 +1,8 @@
 echo "\nINSTALL INKSCAPE START"
 echo "===================================\n"
-sudo apt-get install -y inkscape &&
+#sudo apt-get install -y inkscape &&
+sudo add-apt-repository ppa:inkscape.dev/stable &&
+sudo apt-get update &&
+sudo apt-get install inkscape && 
 echo "\nINSTALL INKSCAPE START"
 echo "===================================\n"
