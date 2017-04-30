5f29b5760e9ee8294b91b4560cb1552614c13a98
Arvid Petermann <divramod@gmail.com>
2017-04-30 06:17:13 +0200

    && && && &&

--- install-all.sh
+++ install-all.sh
@@ -1,11 +1,11 @@
 # [RUN-SCRIPTS]
-wget --no-cache -qO- https://raw.githubusercontent.com/makerspace-eberswalde/dotfiles/master/install-cli.sh | bash
-&& cd ~/.dotfiles
-&& sh install-gui.sh
+wget --no-cache -qO- https://raw.githubusercontent.com/makerspace-eberswalde/dotfiles/master/install-cli.sh | bash &&
+	cd ~/.dotfiles &&
+	sh install-gui.sh &&
 
-# [MESSAGE]
-&& echo "INSTALL SUCCESSFULL!"
+	# [MESSAGE]
+echo "INSTALL SUCCESSFULL!" &&
 
-# [RESTART]
-&& cd ~/.dotfiles
-&& sh scripts/restart.sh
+	# [RESTART]
+cd ~/.dotfiles &&
+	sh scripts/restart.sh
