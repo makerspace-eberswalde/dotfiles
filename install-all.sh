59117f54bbde6f42541d174669a0f49736085006
Arvid Petermann <divramod@gmail.com>
2017-04-30 05:40:00 +0200

    install-all.sh: add eval

--- install-all.sh
+++ install-all.sh
@@ -1,8 +1,8 @@
 # [RUN-SCRIPTS]
 INSTALL-CLI="`wget --no-cache -qO- https://raw.githubusercontent.com/makerspace-eberswalde/dotfiles/master/install-cli.sh`" &&
-	sh $INSTALL_CLI &&
+	eval $INSTALL_CLI &&
 	INSTALL-GUI="`wget --no-cache -qO- https://raw.githubusercontent.com/makerspace-eberswalde/dotfiles/master/install-gui.sh`" &&
-	sh $INSTALL_GUI &&
+	eval $INSTALL_GUI &&
 
 	# [MESSAGE]
 echo "INSTALL SUCCESSFULL!"
