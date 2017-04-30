86e64ec337ddf089033635ce25dabde3640a3681
Arvid Petermann <divramod@gmail.com>
2017-04-30 08:51:08 +0200

    fix gnome-session-fallback error and dropbox install problem

--- install-cli.sh
+++ install-cli.sh
@@ -13,15 +13,12 @@ sudo apt-get update &&
 	sudo apt-get install -y screen &&
 	sudo apt-get install -y openssh-server &&
 	sudo apt-get install -y gnome-panel &&
-	# sudo apt-get install -y gnome-session-fallback &&
+	#sudo apt-get install -y gnome-session-fallback &&
 	sudo apt-get install -y firefox-locale-de &&
 	sudo apt-get install -y thunderbird-locale-de &&
 	sudo apt-get install -y libreoffice-l10n-de &&
 	sudo apt-get install -y libreoffice-help-de &&
 	sudo apt-get install -y flashplugin-installer &&
-	sudo apt-get install -y gstreamer0.10-fluendo-mp3 &&
-	sudo apt-get install -y gstreamer0.10-plugins-bad &&
-	sudo apt-get install -y gstreamer0.10-plugins-ugly &&
 	sudo apt-get install -y openjdk-6-jre &&
 	sudo apt-get install -y nmap &&
 	sudo apt-get install -y wireshark &&
