d10f3cd5f17a7b49a2521f7715554be10ffd999a
Arvid Petermann <divramod@gmail.com>
2017-04-30 09:29:57 +0200

    update

--- gui/dropbox/install.sh
+++ gui/dropbox/install.sh
@@ -3,24 +3,43 @@ echo "\nINSTALL DROPBOX START"
 echo "===================================\n"
 
 # [VERSION3]: https://www.digitalocean.com/community/tutorials/how-to-install-dropbox-client-as-a-service-on-ubuntu-14-04
-#cd ~ &&
-	curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64 &&
-	sudo mkdir -p /opt/dropbox &&
-	sudo tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox &&
+
+cd ~ &&
 	rm -rf dropbox-linux-x86_64.tar.gz &&
-	cd ~ &&
-	sudo curl -o /etc/init.d/dropbox https://gist.githubusercontent.com/thisismitch/d0133d91452585ae2adc/raw/699e7909bdae922201b8069fde3011bbf2062048/dropbox &&
-	sudo chmod +x /etc/init.d/dropbox &&
-	sudo rm /etc/default/dropbox &&
-	sudo touch /etc/default/dropbox &&
-	sudo service dropbox start &&
-	sudo update-rc.d dropbox defaults &&
-	cd ~ &&
-	curl -LO https://www.dropbox.com/download?dl=packages/dropbox.py &&
-	sudo chmod +x ~/dropbox.py &&
-	sudo ln -s /opt/dropbox ~/.dropbox-dist &&
-	~/dropbox.py autostart &&
-	/opt/dropbox/dropboxd &
+	echo "1"
+curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64 &&
+	echo "2"
+sudo mkdir -p /opt/dropbox &&
+	echo "3"
+sudo tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox &&
+	echo "4"
+rm -rf dropbox-linux-x86_64.tar.gz &&
+	echo "5"
+cd ~ &&
+	echo "6"
+sudo curl -o /etc/init.d/dropbox https://gist.githubusercontent.com/thisismitch/d0133d91452585ae2adc/raw/699e7909bdae922201b8069fde3011bbf2062048/dropbox &&
+	echo "7"
+sudo chmod +x /etc/init.d/dropbox &&
+	echo "8"
+sudo rm -rf /etc/default/dropbox &&
+	echo "9"
+sudo touch /etc/default/dropbox &&
+	echo "10"
+sudo service dropbox start &&
+	echo "11"
+sudo update-rc.d dropbox defaults &&
+	echo "12"
+cd ~ &&
+	echo "13"
+curl -LO https://www.dropbox.com/download?dl=packages/dropbox.py &&
+	echo "14"
+sudo chmod +x ~/dropbox.py &&
+	echo "15"
+sudo ln -s /opt/dropbox ~/.dropbox-dist &&
+	echo "16"
+~/dropbox.py autostart y &&
+	echo "17"
+/opt/dropbox/dropboxd &
 
 
 echo "========================================================================================\n"
