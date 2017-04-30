794c6858040ed3356afd9c9d46fa07e55187c2c6
Arvid Petermann <divramod@gmail.com>
2017-04-30 05:43:08 +0200

    zrcs: add dropbox.client & dropbox.deamon

--- gui/dropbox/install.sh
+++ gui/dropbox/install.sh
@@ -5,34 +5,34 @@ echo "===================================\n"
 # [INSTALL-INSTRUCTIONS] https://www.dropbox.com/install 
 # [INSTALL-INSTRUCTIONS] https://www.digitalocean.com/community/tutorials/how-to-install-dropbox-client-as-a-service-on-ubuntu-14-04 
 #cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf - &&
-#~/.dropbox-dist/dropboxd &&
+	#~/.dropbox-dist/dropboxd &&
 
-# [VERSION2]
+	# [VERSION2]
 # [DOWNLOAD]: http://stackoverflow.com/questions/7106012/download-a-single-folder-or-directory-from-a-github-repo 
 #svn checkout https://github.com/makerspace-eberswalde/installer/trunk/dropbox/dropbox &&
-#mv dropbox ~/.dropbox
+	#mv dropbox ~/.dropbox
 #svn checkout https://github.com/makerspace-eberswalde/installer/trunk/dropbox/dropbox-dist &&
-#mv dropbox-dist ~/.dropbox-dist
+	#mv dropbox-dist ~/.dropbox-dist
 #svn checkout https://github.com/makerspace-eberswalde/installer/trunk/dropbox/Dropbox &&
-#mv Dropbox ~/Dropbox
+	#mv Dropbox ~/Dropbox
 #~/.dropbox-dist/dropboxd
 
 # [VERSION3]: https://www.digitalocean.com/community/tutorials/how-to-install-dropbox-client-as-a-service-on-ubuntu-14-04
 #cd ~ &&
-curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64 &&
-sudo mkdir -p /opt/dropbox &&
-sudo tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox &&
-# link to account
+	curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64 &&
+	sudo mkdir -p /opt/dropbox &&
+	sudo tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox &&
+	# link to account
 # Set Up Service Script
 cd ~ &&
-sudo curl -o /etc/init.d/dropbox https://gist.githubusercontent.com/thisismitch/d0133d91452585ae2adc/raw/699e7909bdae922201b8069fde3011bbf2062048/dropbox 2>/dev/null
+	sudo curl -o /etc/init.d/dropbox https://gist.githubusercontent.com/thisismitch/d0133d91452585ae2adc/raw/699e7909bdae922201b8069fde3011bbf2062048/dropbox 2>/dev/null
 sudo chmod +x /etc/init.d/dropbox 2>/dev/null
 sudo rm /etc/default/dropbox
 sudo touch /etc/default/dropbox
 sudo echo "DROPBOX_USERS=\"mkrspcebw\"" >> /etc/default/dropbox
 sudo service dropbox start &&
-sudo update-rc.d dropbox defaults 2>/dev/null &&
-# run droxbox and link
+	sudo update-rc.d dropbox defaults 2>/dev/null &&
+	# run droxbox and link
 /opt/dropbox/dropboxd &
 echo "========================================================================================\n"
 echo "\n\nFirefox should start automatically to allow you Logging into your Dropbox Account!"
