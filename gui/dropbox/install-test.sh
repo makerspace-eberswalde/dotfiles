50d08bbf6f617d67ddbf996b90dc9f2cedc25211
Arvid Petermann <divramod@gmail.com>
2017-04-30 07:22:53 +0200

    update

--- gui/dropbox/install-test.sh
+++ gui/dropbox/install-test.sh
@@ -4,13 +4,13 @@ echo "===================================\n"
 
 # [VERSION3]: https://www.digitalocean.com/community/tutorials/how-to-install-dropbox-client-as-a-service-on-ubuntu-14-04
 #cd ~ &&
-	#curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64 &&
-	#sudo mkdir -p /opt/dropbox &&
-	#sudo tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox &&
-	#rm -rf dropbox-linux-x86_64.tar.gz &&
-	#cd ~ &&
-	#sudo curl -o /etc/init.d/dropbox https://gist.githubusercontent.com/thisismitch/d0133d91452585ae2adc/raw/699e7909bdae922201b8069fde3011bbf2062048/dropbox &&
-	#sudo chmod +x /etc/init.d/dropbox &&
+	curl -Lo dropbox-linux-x86_64.tar.gz https://www.dropbox.com/download?plat=lnx.x86_64 &&
+	sudo mkdir -p /opt/dropbox &&
+	sudo tar xzfv dropbox-linux-x86_64.tar.gz --strip 1 -C /opt/dropbox &&
+	rm -rf dropbox-linux-x86_64.tar.gz &&
+	cd ~ &&
+	sudo curl -o /etc/init.d/dropbox https://gist.githubusercontent.com/thisismitch/d0133d91452585ae2adc/raw/699e7909bdae922201b8069fde3011bbf2062048/dropbox &&
+	sudo chmod +x /etc/init.d/dropbox &&
 	sudo rm /etc/default/dropbox &&
 	sudo touch /etc/default/dropbox &&
 	echo "DROPBOX_USERS=\"mkrspcebw\"" | sudo tee -a /etc/default/dropbox &&
