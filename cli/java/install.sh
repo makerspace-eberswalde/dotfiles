57124e64f82282712c436f0aaf4fe1acbc92f728
divramod <divramod@gmail.de>
2017-05-02 04:30:11 +0200

    java

--- cli/java/install.sh
+++ cli/java/install.sh
@@ -1,8 +1,9 @@
 echo "\nINSTALL JAVA START"
 echo "===================================\n"
 # http://www.webupd8.org/2012/09/install-oracle-java-8-in-ubuntu-via-ppa.html
-sudo add-apt-repository ppa:webupd8team/java &&
+echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections &&
+  sudo add-apt-repository ppa:webupd8team/java -y &&
   sudo apt-get update &&
-  sudo apt-get install oracle-java8-installer
+  sudo apt-get install -y oracle-java8-installer
 echo "\nINSTALL JAVA DONE"
 echo "===================================\n"
