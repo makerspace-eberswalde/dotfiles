a6e44b5fb6efd0a7f3bed6332d1a151e53b9cf75
divramod <divramod@gmail.de>
2017-05-02 05:03:39 +0200

    update

--- gui/virtualbox/install.sh
+++ gui/virtualbox/install.sh
@@ -2,9 +2,10 @@ echo "\nINSTALL VIRTUALBOX START"
 echo "===================================\n"
 # https://askubuntu.com/questions/367248/how-to-install-virtualbox-from-command-line
 # https://askubuntu.com/questions/239892/how-to-install-virtualbox
+# https://askubuntu.com/questions/308237/apt-get-force-yes-how-to-suppress-further-questions
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections &&
-  sudo apt-get install -yyy virtualbox &&
-  sudo apt install -y virtualbox-ext-pack &&
+  yes | sudo apt-get install virtualbox &&
+  yes | sudo apt install virtualbox-ext-pack &&
   sudo adduser $USER vboxusers
 echo "\nINSTALL VIRTUALBOX DONE"
 echo "===================================\n"
