5515210c8d5e984a785bce7cc077f51fa93ea0c5
divramod <divramod@gmail.de>
2017-05-02 04:39:48 +0200

    freemind

--- gui/freemind/install.sh
+++ gui/freemind/install.sh
@@ -1,10 +1,10 @@
 echo "\nINSTALL FREEMIND START"
 echo "===================================\n"
 # https://sourceforge.net/p/freemind/discussion/22102/thread/33f5ff09/
-sudo mkdir /usr/bin/freemind &&
-
-sudo unzip Downloads/freemind-bin-max-1.0.1.zip -d /usr/bin/freemind/
-sudo chmod +x /usr/bin/freemind/freemind.sh
+wget https://raw.githubusercontent.com/makerspace-eberswalde/installer/master/freemind/freemind-bin-max-1.0.1.zip &&
+  sudo mkdir /usr/bin/freemind &&
+  sudo unzip Downloads/freemind-bin-max-1.0.1.zip -d /usr/bin/freemind/ &&
+  sudo chmod +x /usr/bin/freemind/freemind.sh
 
 echo "\nINSTALL FREEMIND END"
 echo "===================================\n"
