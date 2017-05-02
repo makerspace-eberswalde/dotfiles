18c27fe46b246411a39c07f19bebeae00eed9e45
divramod <divramod@gmail.de>
2017-05-02 02:17:36 +0200

    inkscape: configure.sh

--- gui/inkscape/install.sh
+++ gui/inkscape/install.sh
@@ -2,6 +2,9 @@ echo "\nINSTALL INKSCAPE START"
 echo "===================================\n"
 sudo add-apt-repository ppa:inkscape.dev/stable &&
   sudo apt-get update &&
-  sudo apt-get install -y inkscape
+  sudo apt-get install -y inkscape &&
+  cd ~/.dotfiles/gui/inkscape &&
+  sh configure.sh
+
 echo "\nINSTALL INKSCAPE DONE"
 echo "===================================\n"
