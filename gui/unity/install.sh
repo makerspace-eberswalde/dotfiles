5b37fc3d0c795c6e7bc4569efdebdc85043b698a
divramod <divramod@gmail.de>
2017-05-02 03:48:39 +0200

    update

--- gui/unity/install.sh
+++ gui/unity/install.sh
@@ -5,14 +5,15 @@ gsettings set com.canonical.Unity always-show-menus true
 # /usr/share/applications
 # ~/.local/share/applications
 # find pathes: https://ubuntuforums.org/showthread.php?t=1256494
-ln -s ~/.dotfiles/gui/unity/launcher_append_item.py ~/.launcher_append_item.py
-cp ~/.dotfiles/gui/unity/desktop/* ~/.local/share/applications
+ln -s ~/.dotfiles/gui/unity/launcher_append_item.py ~/.launcher_append_item.py &&
+cp ~/.dotfiles/gui/unity/desktop/* ~/.local/share/applications &&
 
-sudo apt-get install -y python-dbus
-python ~/.launcher_append_item.py gnome-terminal.desktop 0
-python ~/.launcher_append_item.py google-chrome.desktop 1
-python ~/.launcher_append_item.py firefox.desktop 2
-python ~/.launcher_append_item.py nautilus-home.desktop 3
-python ~/.launcher_append_item.py gedit.desktop 4
-python ~/.launcher_append_item.py vim.desktop 5
-python ~/.launcher_append_item.py dropbox.desktop 6
+sudo apt-get install -y python-dbus &&
+python ~/.launcher_append_item.py gnome-terminal.desktop 0 &&
+python ~/.launcher_append_item.py google-chrome.desktop 1 &&
+python ~/.launcher_append_item.py firefox.desktop 2 &&
+python ~/.launcher_append_item.py nautilus.desktop 3 &&
+python ~/.launcher_append_item.py gedit.desktop 4 &&
+python ~/.launcher_append_item.py vim.desktop 5 &&
+python ~/.launcher_append_item.py dropbox.desktop 6 &&
+python ~/.launcher_append_item.py inkscape.desktop 7
