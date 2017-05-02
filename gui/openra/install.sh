48f450ea1ef160c31116413c9affccd220b3c861
divramod <divramod@gmail.de>
2017-05-02 05:41:59 +0200

    openra

--- gui/openra/install.sh
+++ gui/openra/install.sh
@@ -6,9 +6,12 @@ echo "===================================\n"
 
 # https://wiki.ubuntuusers.de/GetDeb/
 sudo add-apt-repository 'deb http://archive.getdeb.net/ubuntu xenial-getdeb apps games' &&
+  sudo add-apt-repository ppa:oibaf/graphics-drivers &&
   wget -q http://archive.getdeb.net/getdeb-archive.key -O- | sudo apt-key add - &&
   sudo apt-get update &&
   sudo apt-get install -y libsdl2-dev &&
+  sudo apt-get upgrade &&
+  yes | sudo apt-get install libegl1-mesa libegl1-mesa-drivers xserver-xorg-video-intel libgl1-mesa-dri libglapi-mesa &&
   sudo apt-get install -y openra
 echo "\nINSTALL OPENRA DONE"
 echo "===================================\n"
