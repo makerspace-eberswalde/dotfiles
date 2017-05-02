f91a6a6b2a58d82a3f558f0f35f88b57b42c1e59
divramod <divramod@gmail.de>
2017-05-02 03:38:16 +0200

    add launchers

--- gui/unity/install.sh
+++ gui/unity/install.sh
@@ -2,5 +2,16 @@
 gsettings set com.canonical.Unity always-show-menus true
 
 # https://askubuntu.com/questions/153597/editing-gsettings-add-icon-to-launcher-by-command?noredirect=1&lq=1
+# /usr/share/applications
+# ~/.local/share/applications
+# find pathes: https://ubuntuforums.org/showthread.php?t=1256494
 ln -s ~/.dotfiles/gui/unity/launcher_append_item.py ~/.launcher_append_item.py
+cp ~/.dotfiles/gui/unity/desktop/* ~/.local/share/applications
+
+sudo apt-get install -y python-dbus
 python ~/.launcher_append_item.py gnome-terminal.desktop 0
+python ~/.launcher_append_item.py google-chrome.desktop 1
+python ~/.launcher_append_item.py firefox.desktop 2
+python ~/.launcher_append_item.py nautilus-home.desktop 3
+python ~/.launcher_append_item.py gvim.desktop 4
+python ~/.launcher_append_item.py vim.desktop 5
