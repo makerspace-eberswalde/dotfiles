bdcf92f119253dc343ff45f61697877476aeaf3f
divramod <divramod@gmail.de>
2017-05-02 05:17:57 +0200

    openra

--- gui/openra/install.sh
+++ gui/openra/install.sh
@@ -1,13 +1,14 @@
 echo "\nINSTALL OPENRA START"
 echo "===================================\n"
 # https://wiki.ubuntuusers.de/Spiele/OpenRA/
-#yes | sudo apt-get libSDL2 &&
-  #wget https://raw.githubusercontent.com/makerspace-eberswalde/installer/master/openra/openra_release.20170421_all.deb &&
+#wget https://raw.githubusercontent.com/makerspace-eberswalde/installer/master/openra/openra_release.20170421_all.deb &&
   #yes | sudo apt-get install openra
 
 # https://wiki.ubuntuusers.de/GetDeb/
 sudo add-apt-repository 'deb http://archive.getdeb.net/ubuntu xenial-getdeb apps games' &&
   wget -q http://archive.getdeb.net/getdeb-archive.key -O- | sudo apt-key add - &&
-  sudo apt-get update && sudo apt-get install openra
+  sudo apt-get update &&
+  yes | sudo apt-get libSDL2 &&
+  sudo apt-get install -y openra
 echo "\nINSTALL OPENRA DONE"
 echo "===================================\n"
