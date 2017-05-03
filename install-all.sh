ac35938c17f9830dbc300c2d9993b203bbf9e6c0
divramod <divramod@gmail.de>
2017-05-03 03:09:00 +0200

    mv unity install helper to system

--- install-all.sh
+++ install-all.sh
@@ -2,6 +2,7 @@
 wget --no-cache -qO- https://raw.githubusercontent.com/makerspace-eberswalde/dotfiles/master/install-cli.sh | bash &&
 	cd ~/.dotfiles &&
 	sh install-gui.sh &&
+	sh install-system.sh &&
 
 	# [MESSAGE]
 echo "INSTALL SUCCESSFULL!"
---
+++ install-system.sh
@@ -0,0 +1,6 @@
+  # [INSTALL] unity
+cd ~/.dotfiles/system/unity &&
+  sh install.sh &&
+
+  # [MESSAGE]
+echo "SYSTEM Installation successful!"
