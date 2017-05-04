cc8121d211dd965bf7261deb9fabd30d6ddaeea6
divramod <divramod@gmail.de>
2017-05-04 06:17:40 +0200

    zsh: reorganize to use install.sh and configure.sh

--- cli/zsh/configure.sh
+++ cli/zsh/configure.sh
@@ -12,19 +12,19 @@ rm -f ~/.zshrc_setup &&
 echo "\n.zshrc_setup"
 echo "---------------------\n"
 FILE_RC_SETUP=`pwd`/zshrc_setup &&
-ln -s $FILE_RC_SETUP ~/.zshrc_setup &&
+ln -sf $FILE_RC_SETUP ~/.zshrc_setup &&
 
 # link zshrc_setup which sources zshrc_oh-my-zsh
 echo "\n.zshrc_functions"
 echo "---------------------\n"
 FILE_RC_FUNCTIONS=`pwd`/zshrc_functions &&
-ln -s $FILE_RC_FUNCTIONS ~/.zshrc_functions &&
+ln -sf $FILE_RC_FUNCTIONS ~/.zshrc_functions &&
 
 # link zshrc_setup which sources zshrc_oh-my-zsh
 echo "\n.zshrc_aliases"
 echo "---------------------\n"
 FILE_RC_ALIASES=`pwd`/zshrc_aliases &&
-ln -s $FILE_RC_ALIASES ~/.zshrc_aliases &&
+ln -sf $FILE_RC_ALIASES ~/.zshrc_aliases &&
 
 # cp zshrc which sources zshrc_setup
 # write your own aliases here
