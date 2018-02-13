bd9d01c286744ebc69f00232ec062c69ae5e78e3
divramod <divramod@gmail.de>
2018-02-13 17:43:54 +0100

    add tipp10

---
+++ cli/tipp10/install.sh
@@ -0,0 +1,2 @@
+sudo apt-get install  libxcursor1:i386  libsm6:i386  libxrandr2:i386  libxi6:i386   libxinerama1:i386  libfreetype6:i386  libfontconfig1:i386  libglib2.0-0:i386  libstdc++6:i386
+sudo dpkg -i tipp10_debian_v2-1-0.deb
